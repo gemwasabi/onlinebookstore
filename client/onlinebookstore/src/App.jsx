e3b9e4c30482f8d15ba76ab145306803d87d49f0
Gemi <krasniqigem@gmail.com>
2024-04-09 23:44:48 +0200

    shablloni gati

--- client/onlinebookstore/src/App.jsx
+++ client/onlinebookstore/src/App.jsx
@@ -1,33 +1,55 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Regjistrohu from "./faqet/Regjistrohu";
 import Kycu from "./faqet/Kycu";
 import Libri from "./faqet/Libri";
 import Ballina from "./faqet/Ballina";
+import Navbar from "./komponentet/Navbar";
+import Footer from "./komponentet/Footer";
+
+const Struktura = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Ballina/>,
+    element: <Struktura />,
+    children: [
+      {
+        path: "/",
+        element: <Ballina></Ballina>,
+      },
+      {
+        path: "/libri/:id",
+        element: <Libri></Libri>,
+      },
+    ],
   },
   {
     path: "/regjistrohu",
-    element: <Regjistrohu/>,
+    element: <Regjistrohu />,
   },
   {
     path: "/kycu",
-    element: <Kycu/>,
-  },
-  {
-    path: "/libri",
-    element: <Libri/>,
+    element: <Kycu />,
   },
 ]);
 
 const App = () => {
-    return <div>
+  return (
+    <div className="app">
+      <div className="container">
         <RouterProvider router={router} />
+      </div>
     </div>
+  );
 };
 
 export default App;
