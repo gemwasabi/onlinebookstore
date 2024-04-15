d77551528268d2579bb37ba885da4a29b87ca4ed
Gemi <krasniqigem@gmail.com>
2024-04-15 23:11:19 +0200

    log in page

--- client/src/faqet/Kycu.jsx
+++ client/src/faqet/Kycu.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/img/lype.svg";
 
 const Kycu = () => {
   return (
-    <div>Kycu</div>
-  )
-}
+    <div className="w-screen bg-[#7B8E76]">
+      <div className="flex justify-center">
+        <img
+          className="my-[50px] absolute text-center lg:block hidden"
+          src={logo}
+          alt="Logoja Lype"
+        />
+      </div>
+      <div className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden ">
+        <div className="h-screen w-full rounded-none bg-[#BCC5B8] px-[40px] pb-[30px] shadow-2xl sm:h-auto sm:w-[510px] sm:rounded-2xl">
+          <div className="relative py-[50px]">
+            <input
+              type="text"
+              id="aaa"
+              className="peer h-16 w-full rounded-[10px] border-2 border-[#757C73] bg-inherit px-[16px] text-lg transition-colors duration-100 focus:border-[#51584F] focus:outline-none focus:ring-0"
+              placeholder="Shkruaj Emrin"
+            />
+            <label
+              htmlFor=""
+              className="absolute left-0 z-50 ml-[20px] -translate-y-4 bg-[#BCC5B8] px-3 text-[20px] text-[#757C73] peer-focus:text-[#51584F]"
+            >
+              Emri
+            </label>
+          </div>
+          <div className="relative">
+            <input
+              type="text"
+              id="aaa"
+              className="peer h-16 w-full rounded-[10px] border-2 border-[#757C73] bg-inherit px-[16px] text-lg transition-colors duration-100 focus:border-[#51584F] focus:outline-none focus:ring-0"
+              placeholder="Shkruaj Mbiemrin"
+            />
+            <label
+              htmlFor=""
+              className="absolute left-0 z-50 ml-[20px] -translate-y-4 bg-[#BCC5B8] px-3 text-[20px] text-[#757C73] peer-focus:text-[#51584F]"
+            >
+              Mbiemri
+            </label>
+          </div>
+          <div className="flex justify-between pt-3">
+            <span className="text-lg text-[#757C73]">
+              <input type="checkbox" className="border-none bg-inherit" /> Më
+              mbaj në mend
+            </span>
+            <span className="text-lg text-[#757C73] underline">
+              <a href="#">Ke harruar fjalëkalimin?</a>
+            </span>
+          </div>
+          <div className="flex justify-center">
+            <button className="mt-[116px] w-full rounded-[10px] bg-[#7B8E76] p-2 text-2xl text-[#BCC5B8] transition-all hover:shadow-[inset_0_10px_23px_-15px_rgba(0,0,0,1)]">
+              kyçu
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Kycu
+export default Kycu;
