16c4d4ea92430532b6c580282fc256e122dc2eb8
Gemi <krasniqigem@gmail.com>
2024-04-16 21:17:25 +0200

    rregullimi i gitignore

--- client/src/faqet/Ballina.jsx
+++ client/src/faqet/Ballina.jsx
@@ -17,7 +17,7 @@ const Ballina = () => {
         <div className="relative w-full max-w-[600px]">
           <input
             type="text"
-            className={`w-full rounded-3xl border-4 border-[#ADBBAA] bg-[#BDC6BA] hover:drop-shadow-md p-2 outline-none placeholder:p-2 placeholder:text-left placeholder:font-sans placeholder:text-[#858B83] text-[#747474] px-5 hover:border-[#FFFFFF] hover:shadow active:border-[#FFFFFF] ${isActive ? '' : 'pr-12'}`}
+            className={`transition-colors duration-200 w-full rounded-3xl border-4 border-[#ADBBAA] bg-[#BDC6BA] hover:drop-shadow-md p-2 outline-none placeholder:p-2 placeholder:text-left placeholder:font-sans placeholder:text-[#858B83] text-[#747474] px-5 hover:border-[#FFFFFF] hover:shadow active:border-[#FFFFFF] ${isActive ? '' : 'pr-12'}`}
             placeholder="kërko këtu..."
             onFocus={() => setIsActive(true)}
             onBlur={() => setIsActive(false)} />
