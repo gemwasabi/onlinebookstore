8fe5825f5c0c5bc740e620467d6b9594addd1352
Bardha Kryeziu <bk68313@ubt-uni.net>
2024-04-16 19:09:54 +0200

    landing page

--- client/src/komponentet/Navbar.jsx
+++ client/src/komponentet/Navbar.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import './Navbar.css';
+
 const Navbar = () => {
   
   return (
-    <nav>
-    <div className="logo">
-       <img id="lype" src="https://res.cloudinary.com/dhjdnejbo/image/upload/v1713009973/lype_crhi55.svg" alt=""/>
-    </div>
-    <div className="searchbar">
-        <input type="search" placeholder="kërko këtu.."/> 
-    </div>
-    <div className="items">
-        <img id="user" src="https://res.cloudinary.com/dhjdnejbo/image/upload/v1713122873/User_yr1eko.svg" alt=""/>
-    </div>
-</nav>
+  <div></div>
   );
 };
 
