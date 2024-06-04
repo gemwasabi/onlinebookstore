4b9bda609936b0e1f7e13ad15dc23ccb1d4f5dc8
Fwaka <117106846+AlicentHightower@users.noreply.github.com>
2024-06-04 14:38:17 +0200

    Final Booksearch

---
+++ client/src/komponentet/Booksearch.jsx
@@ -0,0 +1,126 @@
+import React from 'react';
+
+const Booksearch = () => {
+  return (
+    <div className="bg-gray-200 flex justify-center items-center h-screen">
+      <div className="bg-[#7b8f76] w-full h-full p-6 rounded-lg shadow-lg flex flex-col items-center">
+        <div className="relative w-full max-w-xl mb-4">
+          <input
+            type="text"
+            placeholder="kërko këtu..."
+            className="w-full rounded-3xl border-4 border-[#ADBBAA] bg-[#BDC6BA] p-4 outline-none placeholder:p-2 placeholder:text-left placeholder:font-sans placeholder:text-[#858B83]"
+          />
+          <div className="absolute top-2 right-3">
+            <img
+              src="https://res.cloudinary.com/dhjdnejbo/image/upload/v1716846152/Book_1_ti41ba.svg"
+              alt="Search"
+              className="w-8 h-11 text-gray-400"
+            />
+          </div>
+        </div>
+
+        <div className="h-96 overflow-y-auto w-full max-w-xl bg-[#bcc6bb] rounded-lg shadow-lg p-2">
+          <div className="flex justify-between items-center py-4 px-2">
+            <div className="flex items-center">
+              <img
+                src="https://res.cloudinary.com/dhjdnejbo/image/upload/v1717499027/eggli_vlnim8.png"
+                alt="A little life"
+                className="w-16 h-20 mr-4"
+              />
+              <div>
+                <h2 className="text-md text-[#768075] font-bold">A little life</h2>
+                <p className="text-sm text-[#768075]">Nga Hanya Yanagihara</p>
+              </div>
+            </div>
+            <div className="text-md text-[#768075] font-bold">30€</div>
+          </div>
+          <hr className="border-gray-400" />
+          <div className="flex justify-between items-center py-4 px-2">
+            <div className="flex items-center">
+              <img
+                src="https://res.cloudinary.com/dhjdnejbo/image/upload/v1717499027/eggli_vlnim8.png"
+                alt="A little life"
+                className="w-16 h-20 mr-4"
+              />
+              <div>
+                <h2 className="text-md text-[#768075] font-bold">A little life</h2>
+                <p className="text-sm text-[#768075]">Nga Hanya Yanagihara</p>
+              </div>
+            </div>
+            <div className="text-md text-[#768075] font-bold">30€</div>
+          </div>
+          <hr className="border-gray-400" />
+          <div className="flex justify-between items-center py-4 px-2">
+            <div className="flex items-center">
+              <img
+                src="https://res.cloudinary.com/dhjdnejbo/image/upload/v1717499027/eggli_vlnim8.png"
+                alt="A little life"
+                className="w-16 h-20 mr-4"
+              />
+              <div>
+                <h2 className="text-md text-[#768075] font-bold">A little life</h2>
+                <p className="text-sm text-[#768075]">Nga Hanya Yanagihara</p>
+              </div>
+            </div>
+            <div className="text-md text-[#768075] font-bold">30€</div>
+          </div>
+          <hr className="border-gray-400" />
+          <div className="flex justify-between items-center py-4 px-2">
+            <div className="flex items-center">
+              <img
+                src="https://res.cloudinary.com/dhjdnejbo/image/upload/v1717499027/eggli_vlnim8.png"
+                alt="A little life"
+                className="w-16 h-20 mr-4"
+              />
+              <div>
+                <h2 className="text-md text-[#768075] font-bold">A little life</h2>
+                <p className="text-sm text-[#768075]">Nga Hanya Yanagihara</p>
+              </div>
+            </div>
+            <div className="text-md text-[#768075] font-bold">30€</div>
+          </div>
+          <hr className="border-gray-400" />
+          <div className="flex justify-between items-center py-4 px-2">
+            <div className="flex items-center">
+              <img
+                src="https://res.cloudinary.com/dhjdnejbo/image/upload/v1717499027/eggli_vlnim8.png"
+                alt="A little life"
+                className="w-16 h-20 mr-4"
+              />
+              <div>
+                <h2 className="text-md text-[#768075] font-bold">A little life</h2>
+                <p className="text-sm text-[#768075]">Nga Hanya Yanagihara</p>
+              </div>
+            </div>
+            <div className="text-md text-[#768075] font-bold">30€</div>
+          </div>
+          <hr className="border-gray-400" />
+          <div className="flex justify-between items-center py-4 px-2">
+            <div className="flex items-center">
+              <img
+                src="https://res.cloudinary.com/dhjdnejbo/image/upload/v1717499027/eggli_vlnim8.png"
+                alt="A little life"
+                className="w-16 h-20 mr-4"
+              />
+              <div>
+                <h2 className="text-md text-[#768075] font-bold">A little life</h2>
+                <p className="text-sm text-[#768075]">Nga Hanya Yanagihara</p>
+              </div>
+            </div>
+            <div className="text-md text-[#768075] font-bold">30€</div>
+          </div>
+          <hr className="border-gray-400" />
+        </div>
+        <div className="flex justify-center mt-4">
+          <img
+            src="https://res.cloudinary.com/dhjdnejbo/image/upload/v1717499847/Collapse_Arrow_yjyqt0.png"
+            alt="Collapse Arrow"
+            className="w-16 h-16"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Booksearch;
