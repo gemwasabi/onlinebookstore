a2a60448c6299b7864aeee9a9e0a58d92bba547e
Bardha Kryeziu <bardha@gjirafa.com>
2024-11-26 11:47:58 +0100

    Orders in User settings

--- api/routes/porosite.js
+++ api/routes/porosite.js
@@ -10,7 +10,7 @@ import {
 const router = express.Router();
 
 router.get("/", merrPorosite);
-router.get("/porositePerdoruesit/:id", merrPorositePerdoruesit);
+router.get("/:perdoruesi_id", merrPorositePerdoruesit);
 router.post("/pagesa", krijoPagesen);
 router.post("/ruaj-porosine", saveOrder);
 router.get("/invoices/:orderId", getInvoice);
